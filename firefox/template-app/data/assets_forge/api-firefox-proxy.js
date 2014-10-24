331c8c048b8dc63d6ea868aab0091ada70b9d707
Antoine van Gelder <antoine@trigger.io>
2014-10-24 12:38:21 +0200

    Use original content script proxy for Firefox v32 or lower

--- firefox/template-app/data/assets_forge/api-firefox-proxy.js
+++ firefox/template-app/data/assets_forge/api-firefox-proxy.js
@@ -1,5 +1,14 @@
 /* global unsafeWindow, cloneInto, exportFunction, createObjectIn, self */
-unsafeWindow._forge = cloneInto(unsafeWindow._forge || {}, unsafeWindow);
-unsafeWindow._forge.self = createObjectIn(unsafeWindow._forge, {defineAs: "self"});
-exportFunction(self.on, unsafeWindow._forge.self, {defineAs:"on", allowCallbacks:true});
-exportFunction(self.postMessage, unsafeWindow._forge.self, {defineAs:"postMessage", allowCallbacks:true});
+
+var FirefoxMajorVersion = parseInt(navigator.userAgent.match(/(firefox(?=\/))\/?\s*(\d+)/i)[2], 10);
+
+if (FirefoxMajorVersion <= 32) {
+	unsafeWindow._forge = unsafeWindow._forge || {};
+	unsafeWindow._forge.self = self;
+} else {
+	unsafeWindow._forge = cloneInto(unsafeWindow._forge || {}, unsafeWindow);
+	unsafeWindow._forge.self = createObjectIn(unsafeWindow._forge, {defineAs: "self"});
+	exportFunction(self.on, unsafeWindow._forge.self, {defineAs:"on", allowCallbacks:true});
+	exportFunction(self.postMessage, unsafeWindow._forge.self, {defineAs:"postMessage", allowCallbacks:true});
+}
+
